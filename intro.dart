a7a660b1887fee8f57ff40ce8fa833a10ce306ff
ivette <iconstanzav1@gmail.com>
2024-03-15 22:52:25 -0300

    first commit

---
+++ intro.dart
@@ -0,0 +1,10 @@
+void main(){
+  print('Hola Mundo');
+  
+  //Strings
+  var nombre ='Monica';
+  final apellido = 'Geller';
+  
+  nombre = 'Ross';
+  print('$nombre $apellido');//inclui un espacio para que salga en la impresion
+}
