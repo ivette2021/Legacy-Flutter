a62fab86a4bf2e215c844cfb36d295eead6fff3a
ivette <iconstanzav1@gmail.com>
2024-04-06 01:33:24 -0300

    Futures

--- intro.dart
+++ intro.dart
@@ -179,46 +179,62 @@ saludar3(nombre:nombre, mensaje:'aqui va el otro parametro requerido');
 //   void emitirSonido() => print('Miauuuuuuuu');
 // }
 
-abstract class Animal {}
+// abstract class Animal {}
 
-abstract class Mamifero extends Animal {}
+// abstract class Mamifero extends Animal {}
 
-abstract class Ave extends Animal {}
+// abstract class Ave extends Animal {}
 
-abstract class Pez extends Animal {}
+// abstract class Pez extends Animal {}
 
-abstract class Volador {
-  void volar() => print('estoy volando');
-}
+// abstract class Volador {
+//   void volar() => print('estoy volando');
+// }
 
-abstract class Caminante {
-  void caminar() => print('estoy caminando');
-}
+// abstract class Caminante {
+//   void caminar() => print('estoy caminando');
+// }
 
-abstract class Nadador {
-  void nadar() => print('estoy nadando');
-}
+// abstract class Nadador {
+//   void nadar() => print('estoy nadando');
+// }
 
-class Delfin extends Mamifero
-    with Nadador {} //con la palabra with identificamos un mixing
+// class Delfin extends Mamifero
+//     with Nadador {} //con la palabra with identificamos un mixing
 
-class Murcielago extends Mamifero with Caminante, Volador {}
+// class Murcielago extends Mamifero with Caminante, Volador {}
 
-class Gato extends Mamifero with Caminante {}
+// class Gato extends Mamifero with Caminante {}
 
-class Paloma extends Ave with Caminante, Volador {}
+// class Paloma extends Ave with Caminante, Volador {}
 
-class Pato extends Ave with Caminante, Volador, Nadador {}
+// class Pato extends Ave with Caminante, Volador, Nadador {}
 
-class Tiburon extends Pez with Nadador {}
+// class Tiburon extends Pez with Nadador {}
 
-class PezVolador extends Pez with Nadador, Volador {}
+// class PezVolador extends Pez with Nadador, Volador {}
 
+// void main() {
+// //   final flipper = new Delfin();
+// //   flipper.nadar();
+
+// //   final batman = new Murcielago();
+// //   batman.caminar();
+// //   batman.volar();
+// }
+
+//Future
 void main() {
-//   final flipper = new Delfin();
-//   flipper.nadar();
+  //future es una tarea asincrona que se hace a desotiempo es como las corrutinas
+  print('Antes de la petición');
 
-//   final batman = new Murcielago();
-//   batman.caminar();
-//   batman.volar();
+  httpGet('https://api.nasa.com/aliens').then((data) {
+    print(data.toUpperCase());
+  });
+
+  print('Fin del programa');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(Duration(seconds: 3), () => 'Hola Mundo - 3 segundos');
 }
